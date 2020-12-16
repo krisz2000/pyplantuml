dafb6ac517fbaa42f839ece5c944491b36e1f536
Krisztian Schlepp <38430218+krisz2000@users.noreply.github.com>
2020-12-16 11:14:13 +0100

    Fixed compatibility issue
     Fixed compatibility issue with new versions of pylint and astroid

--- pyplantuml/adapter.py
+++ pyplantuml/adapter.py
@@ -1,14 +1,16 @@
 import os
 import sys
 
-from logilab.common.configuration import ConfigurationMixIn
+#from logilab.common.configuration import ConfigurationMixIn
 
 from astroid.manager import AstroidManager
-from astroid.inspector import Linker
 
+from pylint.pyreverse.inspector import Linker, project_from_files
 from pylint.pyreverse.main import Run
 from pylint.pyreverse.diadefslib import DiadefsHandler
 from pylint.pyreverse.utils import insert_default_options
+from pylint.config.configuration_mixin import ConfigurationMixIn
+from pylint.config.options_provider_mixin import OptionsProviderMixIn
 
 
 # TODO: Get package arg and add to PATH as well.
@@ -20,8 +22,10 @@ class PyreverseAdapter(Run):
     def __init__(self, args):
         ConfigurationMixIn.__init__(self, usage=__doc__)
         insert_default_options()
-        self.manager = AstroidManager()
-        self.register_options_provider(self.manager)
+        #self.manager = AstroidManager()
+        #self.manager.priority = -1
+        #self.manager.options = []
+        #self.register_options_provider(self.manager)
         self.args = self.load_command_line_configuration()
 
     def run(self):
@@ -32,7 +36,7 @@ class PyreverseAdapter(Run):
         # dependencies to local modules even if cwd is not in the PYTHONPATH.
         sys.path.insert(0, os.getcwd())
         try:
-            project = self.manager.project_from_files(self.args)
+            project = project_from_files(self.args)
             linker = Linker(project, tag=True)
             handler = DiadefsHandler(self.config)
             diadefs = handler.get_diadefs(project, linker)
